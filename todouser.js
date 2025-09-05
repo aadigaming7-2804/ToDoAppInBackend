7a3edaafea8d2adf43ce989fd70bca2fd8decb48
aadigaming7-2804 <aadigaming.7@gmail.com>
2025-09-05 23:08:45 +0530

    todouser.js

---
+++ todouser.js
@@ -0,0 +1,22 @@
+// todouser.js
+const mongoose = require('./todomongo');
+const bcrypt = require('bcryptjs');
+// Define User schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+const User = mongoose.model('User', userSchema);
+module.exports = User;
